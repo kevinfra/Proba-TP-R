7946f2891c97e6774d81059c938ee8458ac1b966
Ruslan Sobol <rus1147@gmail.com>
2017-06-25 20:15:35 -0300

    InRainbows-ej3corregido

--- tp-R.R
+++ tp-R.R
@@ -1,4 +1,4 @@
-  # Ejercicio 1
+   # Ejercicio 1
   
   estimador_mom_b <- function(n,b,muestra_aleatoria) {
     if (missing(muestra_aleatoria) && !(missing(b) && missing(n))) muestra_aleatoria = runif(n, 0, b)
@@ -22,12 +22,12 @@
   estimador_mom_b_ej3 = estimador_mom_b(n,b,muestra_aleatoria)
   estimador_mv_b_ej3 = estimador_mv_b(n,b,muestra_aleatoria)
   estimador_med_b_ej3 = estimador_med_b(n,b,muestra_aleatoria)
-  print(estimador_mom_b) ; print(estimador_mv_b) ; print(estimador_med_b)
+  print(estimador_mom_b_ej3) ; print(estimador_mv_b_ej3) ; print(estimador_med_b_ej3)
   
-  error_estimador_mom_b = abs(1 - estimador_mom_b_ej3)
-  error_estimador_mv_b = abs(1 - estimador_mv_b_ej3)
-  error_estimador_med_b = abs(1 - estimador_med_b_ej3)
-  print(error_estimador_mom_b) ; print(error_estimador_mv_b) ; print(error_estimador_med_b)
+  error_estimador_mom_b_ej3 = abs(1 - estimador_mom_b_ej3)
+  error_estimador_mv_b_ej3 = abs(1 - estimador_mv_b_ej3)
+  error_estimador_med_b_ej3 = abs(1 - estimador_med_b_ej3)
+  print(error_estimador_mom_b_ej3) ; print(error_estimador_mv_b_ej3) ; print(error_estimador_med_b_ej3)
 
 # Ejercicio 4
 # c)
